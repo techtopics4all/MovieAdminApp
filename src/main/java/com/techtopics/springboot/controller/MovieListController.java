a2db3a3bd7c9a05ba445a8fd44ccf26c74b6131a
techtopics4all <67771295+techtopics4all@users.noreply.github.com>
2020-07-03 00:35:08 -0400

    template updates

--- src/main/java/com/techtopics/springboot/controller/MovieListController.java
+++ src/main/java/com/techtopics/springboot/controller/MovieListController.java
@@ -3,7 +3,6 @@ package com.techtopics.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
